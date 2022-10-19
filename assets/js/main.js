1aeafa40bef3fea48740af63493538bae8c045c6
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-19 12:45:24 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. 
+
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei  numeri da indovinare sono stati individuati.
+
+//Parte il timer di 30 secondi
+
+//Creo i 5 elementi in pagina
+
+//Alla fine dei 30 secondi l'utente deve indovinare tutti e 5 i numeri che c'erano in pagina
+
+
+
